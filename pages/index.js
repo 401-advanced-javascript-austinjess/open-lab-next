b8eccf6bae108eb5e1b424148d3946a579758247
Austin Jess <ajess33@gmail.com>
2019-07-22 14:51:45 -0500

    initial file commit

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Link from 'next/Link';
+
+const Index = () => (
+  <div>
+    <h1>Hello</h1>
+    <Link href="/about">ABOUT PAGE</Link>
+  </div>
+);
+
+export default Index;
